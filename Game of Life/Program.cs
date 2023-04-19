dcc88fb75e0c66f4ee5cb0a88fc8ecc60cb2e270
SebSense <sebastian.senic@gmail.com>
2023-04-19 11:11:31 +0200

    Refactoring, added randomize to game mode

--- Game of Life/Program.cs
+++ Game of Life/Program.cs
@@ -17,7 +17,6 @@ namespace Game_of_Life
         public const int QuitState = 2;
         public static gameBoard gameField;
         public static Coordinate cursor = new();
-        public static int DEFAULT_WIDTH = 40, DEFAULT_HEIGHT = 19;
 
         public class Coordinate
         {
@@ -44,14 +43,9 @@ namespace Game_of_Life
                 for (int j = 0; j < width; j++)
                 {
                     if (lines[i][j] == '1')
-                    {
                         loadedGame.SetActiveTableValue(i, j, true);
-                    }
                     else
-                    {
                         loadedGame.SetActiveTableValue(i, j, false);
-                    }
-
                 }
             }
             return loadedGame;
@@ -80,7 +74,7 @@ namespace Game_of_Life
                 this.width = width;
                 activeTable = new bool[height, width];
                 inactiveTable = new bool[height, width];
-                if (randomize) this.Randomize(height, width);
+                if (randomize) this.Randomize();
             }
             public void ClearActiveTable()
             {
@@ -93,7 +87,7 @@ namespace Game_of_Life
                 inactiveTable = new bool[height, width];
             }
             //Används inte i nuläget:
-            public void Randomize(int height, int width)
+            public void Randomize()
             {
                 Random rand = new Random();
                 for (int i = 0; i < height; i++)
@@ -118,54 +112,120 @@ namespace Game_of_Life
                     output += "\n";
                 }
 
-                output += "\n\n" +
+                output += "\n" +
                     "Controls: \n" +
-                    "spacebar - Runs the simulation one step\n" +
-                    "a        - Runs the simulation until cancelled with spacebar\n" +
-                    "← → ↑ ↓  - Move marker\n" +
-                    "s        - Toggle dead/alive for selected square\n" +
-                    "c        - Clear the gameboard\n" +
-                    "escape   - Return to menu\n";
+                    "← → ↑ ↓  - Move marker                      A - Runs the simulation until cancelled with spacebar\n" +
+                    "SPACEBAR - Runs the simulation one step     R - Randomize the gameboard\n" +
+                    "C        - Clear the gameboard              S - Toggle dead/alive for selected square \n" +
+                    "ESCAPE   - Return to menu\n";
                 Console.WriteLine(output);
                 Console.WindowTop = 0;
             }
             public void calculateGeneration()
             {
                 for (int i = 0; i < height; i++)
-                {
                     for (int j = 0; j < width; j++)
                     {
                         int livecount = 0;
                         for (int x = i - 1; x <= i + 1; x++)
-                        {
                             for (int y = j - 1; y <= j + 1; y++)
                             {
                                 if (x < 0 || y < 0 || x >= height || y >= width || (x == i && y == j)) //denna rad skippar räkna med cellen man står på och cellerna utanför rutnätet
-                                {
                                     continue;
-                                }
                                 if (activeTable[x, y] == true)
-                                {
                                     livecount++;
-                                }
                             }
-                        }
-                        if (activeTable[i, j] == false && livecount == 3) //Döda celler med 3 eller fler grannar återupplivas
-                        {
+                        if (activeTable[i, j] == false && livecount == 3) //Döda celler med 3 grannar återupplivas
                             inactiveTable[i, j] = true;
-                        }
                         else if (activeTable[i, j] == true && (livecount == 2 || livecount == 3)) //Levande celler med 2 eller 3 grannar lever vidare
-                        {
                             inactiveTable[i, j] = true;
-                        }
                         else
-                        {
                             inactiveTable[i, j] = false; //Celler med none of the above fortsätter vara döda
-                        }
                     }
+            }
+        }
+        public class Menu
+        {
+            private string[] Options;
+            private int SelectedOption;
+            private readonly string selectedPrefixMarker = "-> ";
+
+            public Menu()
+            {
+                SelectedOption = 0;
+                Options = new string[] { "New Game", "Load Game", "Quit" };
+            }
+            public void PrintMenu()
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.Clear();
+                Console.CursorVisible = false;
+                string emptySpace = "";
+                for (int i = 0; i < selectedPrefixMarker.Length; i++)
+                {
+                    emptySpace += " ";
+                }
+
+                for (int i = 0; i < Options.Length; i++)
+                {
+                    if (i == SelectedOption)
+                    {
+                        Console.WriteLine(selectedPrefixMarker + Options[i]);
+                    }
+                    else
+                    {
+
+                        Console.WriteLine(emptySpace + Options[i]);
+                    }
+                }
+                Console.ResetColor();
+            }
+            private void IncrementSelectedOption()
+            {
+                if (SelectedOption < Options.Length - 1)
+                {
+                    SelectedOption++;
+                }
+            }
+            private void DecrementSelectedOption()
+            {
+                if (SelectedOption > 0)
+                {
+                    SelectedOption--;
+                }
+            }
+            public void checkInput()
+            {
+                ConsoleKeyInfo KeyInfo = Console.ReadKey();
+                if (KeyInfo.Key.ToString() == "UpArrow") DecrementSelectedOption();
+                if (KeyInfo.Key.ToString() == "DownArrow") IncrementSelectedOption();
+                if (KeyInfo.Key.ToString() == "Enter") ApplySelectedOption();
+
+            }
+            public void ApplySelectedOption()
+            {
+                if (SelectedOption == 0)
+                {
+                    cursor.Reset();
+                    gameField = new gameBoard(22, 40);
+                    Console.WindowTop = 0;
+                    Program.CurrentState = Program.GameState;
+                }
+                if (SelectedOption == 1)
+                {
+                    // ladda nytt spel från fil
+                    cursor.Reset();
+                    string filePath = Path.GetFullPath("..\\..\\..\\gameoflife2.txt");
+                    Program.gameField = Program.LoadGameFromFile(filePath);
+                    Program.CurrentState = Program.GameState;
+                }
+                if (SelectedOption == 2)
+                {
+                    Program.CurrentState = Program.QuitState;
                 }
             }
         }
+
         static void Main(string[] args)
         {
             CurrentState = MenuState;
@@ -187,7 +247,6 @@ namespace Game_of_Life
                     if (KeyInfo.Key.ToString() == "Spacebar")
                     {
                         gameField.Step();
-                        gameField.PrintTable();
                     }
                     else if (KeyInfo.Key.ToString() == "Escape")//FIXME: När man kör load game och sen escape så ser menyn lite konstigt ut
                     {
@@ -197,22 +256,18 @@ namespace Game_of_Life
                     else if (KeyInfo.Key.ToString() == "RightArrow" && cursor.x < gameField.width - 1)
                     {
                         cursor.x++;
-                        gameField.PrintTable();
                     }
                     else if (KeyInfo.Key.ToString() == "LeftArrow" && cursor.x > 0)
                     {
                         cursor.x--;
-                        gameField.PrintTable();
                     }
                     else if (KeyInfo.Key.ToString() == "DownArrow" && cursor.y < gameField.height - 1)
                     {
                         cursor.y++;
-                        gameField.PrintTable();
                     }
                     else if (KeyInfo.Key.ToString() == "UpArrow" && cursor.y > 0)
                     {
                         cursor.y--;
-                        gameField.PrintTable();
                     }
                     else if (KeyInfo.Key.ToString().ToLower() == "s")
                     {
@@ -221,6 +276,10 @@ namespace Game_of_Life
                     else if (KeyInfo.Key.ToString().ToLower() == "c")
                     {
                         gameField.ClearActiveTable();
+                    }
+                    else if (KeyInfo.Key.ToString().ToLower() == "r")
+                    {
+                        gameField.Randomize();
                     }
                     else if (KeyInfo.Key.ToString().ToLower() == "a")
                     {
@@ -233,87 +292,6 @@ namespace Game_of_Life
                     }
 
                 }
-            }
-        }
-    }
-    public class Menu
-    {
-        private string[] Options;
-        private int SelectedOption;
-        private readonly string selectedPrefixMarker = "-> ";
-
-        public Menu()
-        {
-            SelectedOption = 0;
-            Options = new string[] { "New Game", "Load Game", "Quit" };
-        }
-        public void PrintMenu()
-        {
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.Clear();
-            Console.CursorVisible = false;
-            string emptySpace = "";
-            for (int i = 0; i < selectedPrefixMarker.Length; i++)
-            {
-                emptySpace += " ";
-            }
-
-            for (int i = 0; i < Options.Length; i++)
-            {
-                if (i == SelectedOption)
-                {
-                    Console.WriteLine(selectedPrefixMarker + Options[i]);
-                }
-                else
-                {
-
-                    Console.WriteLine(emptySpace + Options[i]);
-                }
-            }
-            Console.ResetColor();
-        }
-        private void IncrementSelectedOption()
-        {
-            if (SelectedOption < Options.Length - 1)
-            {
-                SelectedOption++;
-            }
-        }
-        private void DecrementSelectedOption()
-        {
-            if (SelectedOption > 0)
-            {
-                SelectedOption--;
-            }
-        }
-        public void checkInput()
-        {
-            ConsoleKeyInfo KeyInfo = Console.ReadKey();
-            if (KeyInfo.Key.ToString() == "UpArrow") DecrementSelectedOption();
-            if (KeyInfo.Key.ToString() == "DownArrow") IncrementSelectedOption();
-            if (KeyInfo.Key.ToString() == "Enter") ApplySelectedOption();
-
-        }
-        public void ApplySelectedOption()
-        {
-            if (SelectedOption == 0)
-            {
-                cursor.Reset();
-                gameField = new gameBoard(DEFAULT_HEIGHT, DEFAULT_WIDTH);
-                Console.WindowTop = 0;
-                Program.CurrentState = Program.GameState;
-            }
-            if (SelectedOption == 1)
-            {
-                // ladda nytt spel från fil
-                cursor.Reset();
-                string filePath = Path.GetFullPath("..\\..\\..\\gameoflife2.txt");
-                Program.gameField = Program.LoadGameFromFile(filePath);
-                Program.CurrentState = Program.GameState;
-            }
-            if (SelectedOption == 2)
-            {
-                Program.CurrentState = Program.QuitState;
             }
         }
     }
